25a26b9a46bdaa548a5010e16bb6c58a97f195d6
Erica Layton <e.alexislayton@gmail.com>
2022-11-22 13:05:35 -0800

    fix innerHTML in main.js

--- main.js
+++ main.js
@@ -47,9 +47,6 @@ function fetchIssues() {
         "')\">Reopen</a> " +
         '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' +
         id +
-        "')\">Close</a> " +
-        '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' +
-        id +
         "')\">Delete</a> " +
         "</div>";
     } else {
